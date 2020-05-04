6f37506d334b8b21cd38d706903885577ab689dc
liwasai <lcm472437013@qq.com>
2020-05-04 11:00:06 +0800

    '创建账号'

--- common/libs/user/UserService.py
+++ common/libs/user/UserService.py
@@ -1,7 +1,7 @@
-import hashlib,base64
+import hashlib,base64,random,string
 class UserService():
     
-    # 结合salt和md5 生成新的密码
+    # 结合salt和md5 生成加密的密码
     @staticmethod
     def generatePwd(pwd,salt):
         m = hashlib.md5()
@@ -16,3 +16,12 @@ class UserService():
         str = "%s-%s-%s-%s"%(user_info.uid,user_info.login_name,user_info.login_pwd,user_info.login_salt)
         m.update(str.encode("utf-8"))
         return m.hexdigest()
+
+    # 生成16位的字符串，包含字母和数字
+    # string.ascii_letters  所有的大小写字母
+    # string.digits 0-9 数字
+    @staticmethod
+    def generateSalt(length=16):
+        keyList = [ random.choice(( string.ascii_letters + string.digits )) for i in range(length) ]
+
+        return ("".join(keyList))
---
+++ web/static/js/account/set.js
@@ -0,0 +1,13 @@
+;
+var account_set_ops = {
+    init:function(){
+        this.eventBind()
+    },
+    eventBind:function(){
+
+    }
+}
+
+$(document).ready(function(){
+    account_set_ops.init()
+})
